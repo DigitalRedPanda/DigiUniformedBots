c4a3ce812132a03f9d0b7daf3c9a457b53d7d793
DigitalRedPanda <osama-12341905@hotmail.com>
2022-02-07 03:20:19 +0300

    Preparation for retrieving previous project with additional features and services alongside bug fixes

--- src/main/java/TwitchBot/TwitchBotResources.java
+++ src/main/java/TwitchBot/TwitchBotResources.java
@@ -7,4 +7,12 @@ public interface TwitchBotResources {
             .directory("src\\java\\main\\resources")
             .filename(".env")
             .load();
+    String DefaultChannel = dotenv.get("DEFAULT_CHANNEL");
+    String Token = dotenv.get("TOKEN");
+    String ClientSecret = dotenv.get("CLIENT_SECRET");
+    String ClientID = dotenv.get("CLIENT_ID");
+    String RedirectURL = dotenv.get("REDIRECT_URL");
+    String SpecialGuest = dotenv.get("SPECIAL_GUEST");
+
+
 }
