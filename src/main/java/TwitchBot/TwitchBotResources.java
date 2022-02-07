25589d370373fe1cc193d19bcb41d1586d5bb9c4
DigitalRedPanda <osama-12341905@hotmail.com>
2022-02-07 03:02:54 +0300

    Preparation for retrieving previous project with additional features and services

--- src/main/java/TwitchBot/TwitchBotResources.java
+++ src/main/java/TwitchBot/TwitchBotResources.java
@@ -1,4 +1,10 @@
 package TwitchBot;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public interface TwitchBotResources {
+    Dotenv dotenv = Dotenv.configure()
+            .directory("src\\java\\main\\resources")
+            .filename(".env")
+            .load();
 }
